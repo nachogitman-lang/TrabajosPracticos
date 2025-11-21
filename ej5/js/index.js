3a1d4121bcaabb12ded2448e3c4fd3020ec73a60
Ignacio <nachogitman@gmail.com>
2025-11-21 19:05:25 -0300

    resultado final

--- ej5/js/index.js
+++ ej5/js/index.js
@@ -1,29 +1,40 @@
 
-//Ejercicio 1
+//Ejercicio Nº 1 del TP5
 
-let cantidadDeGatos = 5;
+let cantidadDeGatos = parseInt(prompt("Ingrese el número de gatos", "Ej. 10"));
 
-const emojis = ['😺', '😸', '😹'];
+let gato1 = "😺";
+let gato2 = "😸";
+let gato3 = "😹";
 
-for (let i = 1; i <= cantidadDeGatos; i++) {
-    console.log(`Gato #${i}: ${emojis[(i - 1) % emojis.length]}`);
-}
-
-//Ejercicio 2
-
-let cantidadDePasos = 8;
-const gatoEmoji = '🐈';
-const pasoEmoji = '🐾';
-
-
-for (let i = 1; i <= cantidadDeGatos; i++) {
-    console.log(`Gato #${i}: ${gatoEmoji} ${pasoEmoji.repeat(cantidadDePasos)}`);
-}
-
-//Ejercicio 3
-
-const gatos =  ['🐈', '🐈‍⬛'];
-
-for (let i = 1; i <= cantidadDeGatos; i++) {
-    console.log(`Gato #${i}: ${gatos[(i - 1) % gatos.length]} ${pasoEmoji.repeat(cantidadDePasos)}`);
+for (let i = 1; i <= cantidadDeGatos; i++){
+    if (cantidadDeGatos === 1 || (i-1)%3 === 0){
+        console.log("Gato #" + i + ":" + " " + gato1);
+    } else if (cantidadDeGatos === 2 || (i-2)%3 === 0){
+        console.log("Gato #" + i + ":" + " " + gato2);
+    } else if(cantidadDeGatos === 3 || (i-3)%3 === 0)
+        console.log("Gato #" + i + ":" + " " + gato3);
     }
+
+//Ejercicio Nº 2 del TP5
+
+let cantidadDePasos = parseInt(prompt("Ingrese la cantidad de pasos", "Ej. 5"));
+
+let gatito1 = "🐈";
+let pasos = "🐾";
+
+for (let i = 1; i <= cantidadDeGatos; i++){
+    console.log("Gato #" + i + ":" + gatito1 + " " + pasos.repeat(cantidadDePasos));
+}
+
+//Ejercicio Nº 3 del TP5
+
+let gatito2 = "🐈‍⬛";
+
+for (let i = 1;i <= cantidadDeGatos; i++){
+    if (i%2 === 0){
+        console.log("Gato #" + i + ":" + gatito2 + " " + pasos.repeat(cantidadDePasos));
+    } else {
+        console.log("Gato #" + i + ":" + gatito1 + " " + pasos.repeat(cantidadDePasos));
+    }
+}
