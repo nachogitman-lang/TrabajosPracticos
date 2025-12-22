a23418913ff156a6dc6ee8455dbf846d87ab7261
Ignacio <nachogitman@gmail.com>
2025-12-22 15:58:08 -0300

    agreado un alert al alta

---
+++ Parcial/js/alta.js
@@ -0,0 +1,27 @@
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+    
+    const camposRequeridos = formulario.querySelectorAll("[required]");
+    let todosLlenos = true;
+
+    camposRequeridos.forEach(campo => {
+        
+        if (campo.value.trim() === "") {
+            todosLlenos = false;
+        }
+    });
+
+    if (!todosLlenos) {
+        alert("Por favor, completa todos los campos.");
+    } else {
+        alert("Alta de producto con éxito");
+        
+    }
+});
+    
+    
+    
+    
